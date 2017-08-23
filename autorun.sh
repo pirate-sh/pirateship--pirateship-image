28ee8481c3295da708c1370da79445b08cacc097
dogi <stefan@unterhauser.name>
2017-08-23 17:54:44 -0400

    treehouse images

--- autorun.sh
+++ autorun.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+type='latest'
+#type='stable'
+
 echo timer > /sys/class/leds/led0/trigger
 cd /data
 rm -f new.sha1
-if wget http://pirate.sh/latest-pirateship.img.gz.sha1 -O new.sha1; then
-  if [[ ! -e latest-pirateship.img.gz ]] || [[ ! -e latest-pirateship.img.gz.sha1 ]] || [[ `cat new.sha1` != `cat latest-pirateship.img.gz.sha1` ]]; then
-    rm -f latest-pirateship.img.gz
-    wget http://pirate.sh/latest-pirateship.img.gz
-    rm -f latest-pirateship.img.gz.sha1
-    wget http://pirate.sh/latest-pirateship.img.gz.sha1
+if wget http://dev.ole.org/$type.img.gz.sha1 -O new.sha1; then
+  if [[ ! -e $type.img.gz ]] || [[ ! -e $type.img.gz.sha1 ]] || [[ `cat new.sha1` != `cat $type.img.gz.sha1` ]]; then
+    rm -f $type.img.gz
+    wget http://dev.ole.org/$type.img.gz
+    rm -f $type.img.gz.sha1
+    wget http://dev.ole.org/$type.img.gz.sha1
   fi
 fi
 
 echo heartbeat > /sys/class/leds/led0/trigger
 if [[ -b /dev/sdb ]] ; then
-  if [[ -e latest-pirateship.img.gz ]]; then
-    zcat latest-pirateship.img.gz > /dev/sdb
+  if [[ -e $type.img.gz ]]; then
+    zcat $type.img.gz > /dev/sdb
   else
     echo u > /proc/sysrq-trigger
     dd if=/dev/mmcblk0 bs=1M of=/dev/sdb count=4K
